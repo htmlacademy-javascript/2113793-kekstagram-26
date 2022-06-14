a190325499d5d5a9658137dd7318ba62d9bbd58e
toaigel <toaigel@gmail.com>
2022-06-14 15:30:12 +0300

    добавлена функция getPhotoDescription

--- js/main.js
+++ js/main.js
@@ -1,46 +1,88 @@
-/**
- * Возвращает случайное целое число в заданном интервале
- * @param {number} a - число из диапазона
- * @param {number} b - число из диапазона
- * @return {integer} - случайное число
- * src: https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
-*/
-
-function getRandomPositiveInteger (a, b) {
-  // Чтобы не заставлять пользователя нашей функции помнить порядок аргументов,
-  // реализуем поддержку передачи минимального и максимального значения в любом порядке,
-  // а какое из них большее и меньшее вычислим с помощью Math.min и Math.max.
-
-  // После нам нужно убедиться, что пользователь не передал дробные значения,
-  // для этого на всякий случай нижнюю границу диапазона
-  // мы округляем к ближайшему большему целому с помощью Math.ceil,
-  // а верхнюю границу - к ближайшему меньшему целому с помощью Math.floor
+const getRandomPositiveInteger = (a, b) => {
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-  // Обратите внимание, чтобы учесть условие, что диапазон может быть [0, ∞),
-  // мы не ругаем пользователя за переданное отрицательное число,
-  // а просто берём его по модулю с помощью Math.abs
-
-  // Дальше используем Math.random() для получения случайного дробного числа в диапазоне [0, 1),
-  // которое домножаем на разницу между переданными числами плюс единица - это будет наша случайная дельта.
-  // После нужно сложить дельту с минимальным значением, чтобы получить итоговое случайное число.
   const result = Math.random() * (upper - lower + 1) + lower;
-  // "Плюс единица", чтобы включить верхнюю границу диапазона в случайные числа
-
-  // И в конце с помощью метода Math.floor мы округляем полученный результат,
-  // потому что Math.random() генерирует только дробные числа и ноль.
   return Math.floor(result);
-}
-getRandomPositiveInteger(34,15);
+};
 
-/**
-   * Проверяет максимальную длину строки
-   * @param {string} stringToBeChecked - проверяемая строка
-   * @param {number} maxLenth - максимальная длина строки
-   * @return {boolean} - true, если строка проходит по длине, и false — если не проходит
-   * src: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-   * src2: https://codepen.io/jsundai/pen/vYKxNZp
-  */
+const DESCRIPTION = [
+  'summer vibes',
+  'entrepreneur life',
+  'We did it!',
+  'Yes or No?',
+  'life’s a beach',
+  'hello sunshine!',
+  'Only God Can Judge Me',
+  'California love'
+];
 
-const checkLenth = (stringToBeChecked, maxLenth) => (stringToBeChecked.length <= maxLenth);
-checkLenth('Hello world!', 10);
+const MESSAGE = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.'
+];
+
+const NAME = [
+  'Olivia',
+  'Lucas',
+  'Emma',
+  'Oliver',
+  'Theodore',
+  'Mia',
+  'Isabella',
+  'James'
+];
+
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
+
+
+const getPhotoDescription = () => {
+
+  const getCommentId = () => {
+    const commentIds = [];
+    for (let i = 1; i <= 50; i++) {
+      commentIds.push(i);
+    }
+    const idIndex = getRandomPositiveInteger(1, commentIds.length);
+    const commentId = commentIds[idIndex];
+    commentIds.splice(idIndex);
+    return commentId;
+  };
+  const getPhotoId = () => {
+    const photoIds = [];
+    for (let i = 1; i <= 25; i++) {
+      photoIds.push(i);
+    }
+    const idIndex = getRandomPositiveInteger(1, photoIds.length);
+    const photoId = photoIds[idIndex];
+    photoIds.splice(idIndex);
+    return photoId;
+  };
+
+  const photoComments = [
+    {
+      id: getCommentId(),
+      avatar: `img/avatar-${getRandomPositiveInteger(1,6)}.svg`,
+      message: getRandomArrayElement(MESSAGE),
+      name: getRandomArrayElement(NAME)
+    },
+    {
+      id: getCommentId(),
+      avatar: `img/avatar-${getRandomPositiveInteger(1,6)}.svg`,
+      message: getRandomArrayElement(MESSAGE),
+      name: getRandomArrayElement(NAME)
+    }
+  ];
+  return {
+    id: getPhotoId(),
+    url: `photos/${getRandomPositiveInteger(1,25)}.jpg`,
+    description: getRandomArrayElement(DESCRIPTION),
+    likes: getRandomPositiveInteger(15,200),
+    comments: photoComments
+  };
+};
+
+const photoDescriptions = Array.from({length:25},getPhotoDescription);
+
+console.log(photoDescriptions);
